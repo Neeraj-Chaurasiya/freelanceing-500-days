653b6dfea2a5de9d323588f691cdcc2b8836525d
Neeraj <your-email@example.com>
2026-01-01 23:11:59 +0530

    day 48 updated small changes

--- Month_02/day_48/python_pipeline.py
+++ Month_02/day_48/python_pipeline.py
@@ -1,1 +1,14 @@
-print("Hello, World!")
+import pandas as pd 
+
+df = pd.read_csv(r"C:\Freelanceing (500 days)\Month_02\day_48\Data\ecommerce_sales_34500.csv")
+
+df['order_date'] = pd.to_datetime(df['order_date'])
+
+df['final_amount'] = df['price']*df['quantity']*(1-df['discount'])
+
+summary = df.groupby('region')['final_amount'].sum().reset_index()
+print(summary)
+
+df.to_csv(r"C:\Freelanceing (500 days)\Month_02\day_48\Data\processed_sales.csv", index=False)
+summary.to_csv(r"C:\Freelanceing (500 days)\Month_02\day_48\Data\sales_summary_by_region.csv", index=False)
+print("Data processing complete. Processed files saved.")
